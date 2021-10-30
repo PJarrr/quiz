8ab9dd8f5ad7002364f3e95e56f314d9c84d945f
Paulius Jarutis <76951786+PJarrr@users.noreply.github.com>
2021-10-30 14:24:58 +0300

    seed test

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -59,77 +59,78 @@ class DatabaseSeeder extends Seeder
             'incorrect_answer3'=>'4',
             
         ]);
-         DB::table('questions')->insert([
-            
-            'user_id' => 2,
-            'question_text'=>'Kas?', 
-            'correct_answer'=>'correct1',
-            'incorrect_answer1'=>'2',
-            'incorrect_answer2'=>'3',
-            'incorrect_answer3'=>'4',
-            
-        ]);
-         DB::table('questions')->insert([
-            
-            'user_id' => 2,
-            'question_text'=>'Kodel?', 
-            'correct_answer'=>'correct2',
-            'incorrect_answer1'=>'2',
-            'incorrect_answer2'=>'3',
-            'incorrect_answer3'=>'4',
-            
-        ]);
-         DB::table('questions')->insert([
-            
-            'user_id' => 2,
-            'question_text'=>'Kaip?', 
-            'correct_answer'=>'correct3',
-            'incorrect_answer1'=>'2',
-            'incorrect_answer2'=>'3',
-            'incorrect_answer3'=>'4',
-            
-        ]);
 
-         DB::table('quizzes')->insert([
-            'user_id' => 1,
-            'title' => 'quiz1',
-            'password'=>'123',
-            'description' =>'test quiz',
-            'time'=>1,
-        ]);
-         DB::table('quizzes')->insert([
-            'user_id' => 2,
-            'title' => 'quiz2',
-            'password'=>'123',
-            'description' =>'test quiz',
-            'time'=>1,
-        ]);
+        //  DB::table('questions')->insert([
+            
+        //     'user_id' => 2,
+        //     'question_text'=>'Kas?', 
+        //     'correct_answer'=>'correct1',
+        //     'incorrect_answer1'=>'2',
+        //     'incorrect_answer2'=>'3',
+        //     'incorrect_answer3'=>'4',
+            
+        // ]);
+        //  DB::table('questions')->insert([
+            
+        //     'user_id' => 2,
+        //     'question_text'=>'Kodel?', 
+        //     'correct_answer'=>'correct2',
+        //     'incorrect_answer1'=>'2',
+        //     'incorrect_answer2'=>'3',
+        //     'incorrect_answer3'=>'4',
+            
+        // ]);
+        //  DB::table('questions')->insert([
+            
+        //     'user_id' => 2,
+        //     'question_text'=>'Kaip?', 
+        //     'correct_answer'=>'correct3',
+        //     'incorrect_answer1'=>'2',
+        //     'incorrect_answer2'=>'3',
+        //     'incorrect_answer3'=>'4',
+            
+        // ]);
 
-         DB::table('question_quiz')->insert([
-            'quiz_id' => 1,
-            'question_id' => 1,
-        ]);
-         DB::table('question_quiz')->insert([
-            'quiz_id' => 1,
-            'question_id' => 2,
-        ]);
-         DB::table('question_quiz')->insert([
-            'quiz_id' => 1,
-            'question_id' => 3,
-        ]);
+        //  DB::table('quizzes')->insert([
+        //     'user_id' => 1,
+        //     'title' => 'quiz1',
+        //     'password'=>'123',
+        //     'description' =>'test quiz',
+        //     'time'=>1,
+        // ]);
+        //  DB::table('quizzes')->insert([
+        //     'user_id' => 2,
+        //     'title' => 'quiz2',
+        //     'password'=>'123',
+        //     'description' =>'test quiz',
+        //     'time'=>1,
+        // ]);
 
-         DB::table('question_quiz')->insert([
-            'quiz_id' => 2,
-            'question_id' => 4,
-        ]);
-         DB::table('question_quiz')->insert([
-            'quiz_id' => 2,
-            'question_id' => 5,
-        ]);
-         DB::table('question_quiz')->insert([
-            'quiz_id' => 2,
-            'question_id' => 6,
-        ]);
+        //  DB::table('question_quiz')->insert([
+        //     'quiz_id' => 1,
+        //     'question_id' => 1,
+        // ]);
+        //  DB::table('question_quiz')->insert([
+        //     'quiz_id' => 1,
+        //     'question_id' => 2,
+        // ]);
+        //  DB::table('question_quiz')->insert([
+        //     'quiz_id' => 1,
+        //     'question_id' => 3,
+        // ]);
+
+        //  DB::table('question_quiz')->insert([
+        //     'quiz_id' => 2,
+        //     'question_id' => 4,
+        // ]);
+        //  DB::table('question_quiz')->insert([
+        //     'quiz_id' => 2,
+        //     'question_id' => 5,
+        // ]);
+        //  DB::table('question_quiz')->insert([
+        //     'quiz_id' => 2,
+        //     'question_id' => 6,
+        // ]);
 
         
 
