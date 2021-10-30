8ba15fee2dc3133b7b1dd7081d3bad79ecb37d3e
Paulius Jarutis <76951786+PJarrr@users.noreply.github.com>
2021-10-30 14:36:01 +0300

    seeds to clear db

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -31,7 +31,7 @@ class DatabaseSeeder extends Seeder
 
          DB::table('questions')->insert([
             
-            'user_id' => 1,
+            'user_id' => 5,
             'question_text'=>'Kas?', 
             'correct_answer'=>'correct1',
             'incorrect_answer1'=>'2',
@@ -41,7 +41,7 @@ class DatabaseSeeder extends Seeder
         ]);
          DB::table('questions')->insert([
             
-            'user_id' => 1,
+            'user_id' => 5,
             'question_text'=>'Kodel?', 
             'correct_answer'=>'correct2',
             'incorrect_answer1'=>'2',
@@ -51,7 +51,7 @@ class DatabaseSeeder extends Seeder
         ]);
          DB::table('questions')->insert([
             
-            'user_id' => 1,
+            'user_id' => 5,
             'question_text'=>'Kaip?', 
             'correct_answer'=>'correct3',
             'incorrect_answer1'=>'2',
@@ -60,77 +60,77 @@ class DatabaseSeeder extends Seeder
             
         ]);
 
-        //  DB::table('questions')->insert([
+         DB::table('questions')->insert([
             
-        //     'user_id' => 2,
-        //     'question_text'=>'Kas?', 
-        //     'correct_answer'=>'correct1',
-        //     'incorrect_answer1'=>'2',
-        //     'incorrect_answer2'=>'3',
-        //     'incorrect_answer3'=>'4',
+            'user_id' => 15,
+            'question_text'=>'Kas?', 
+            'correct_answer'=>'correct1',
+            'incorrect_answer1'=>'2',
+            'incorrect_answer2'=>'3',
+            'incorrect_answer3'=>'4',
             
-        // ]);
-        //  DB::table('questions')->insert([
+        ]);
+         DB::table('questions')->insert([
             
-        //     'user_id' => 2,
-        //     'question_text'=>'Kodel?', 
-        //     'correct_answer'=>'correct2',
-        //     'incorrect_answer1'=>'2',
-        //     'incorrect_answer2'=>'3',
-        //     'incorrect_answer3'=>'4',
+            'user_id' => 15,
+            'question_text'=>'Kodel?', 
+            'correct_answer'=>'correct2',
+            'incorrect_answer1'=>'2',
+            'incorrect_answer2'=>'3',
+            'incorrect_answer3'=>'4',
             
-        // ]);
-        //  DB::table('questions')->insert([
+        ]);
+         DB::table('questions')->insert([
             
-        //     'user_id' => 2,
-        //     'question_text'=>'Kaip?', 
-        //     'correct_answer'=>'correct3',
-        //     'incorrect_answer1'=>'2',
-        //     'incorrect_answer2'=>'3',
-        //     'incorrect_answer3'=>'4',
+            'user_id' => 15,
+            'question_text'=>'Kaip?', 
+            'correct_answer'=>'correct3',
+            'incorrect_answer1'=>'2',
+            'incorrect_answer2'=>'3',
+            'incorrect_answer3'=>'4',
             
-        // ]);
+        ]);
 
-        //  DB::table('quizzes')->insert([
-        //     'user_id' => 1,
-        //     'title' => 'quiz1',
-        //     'password'=>'123',
-        //     'description' =>'test quiz',
-        //     'time'=>1,
-        // ]);
-        //  DB::table('quizzes')->insert([
-        //     'user_id' => 2,
-        //     'title' => 'quiz2',
-        //     'password'=>'123',
-        //     'description' =>'test quiz',
-        //     'time'=>1,
-        // ]);
+         DB::table('quizzes')->insert([
+            'user_id' => 5,
+            'title' => 'quiz1',
+            'password'=>'123',
+            'description' =>'test quiz',
+            'time'=>1,
+        ]);
+         DB::table('quizzes')->insert([
+            'user_id' => 15,
+            'title' => 'quiz2',
+            'password'=>'123',
+            'description' =>'test quiz',
+            'time'=>1,
+        ]);
 
-        //  DB::table('question_quiz')->insert([
-        //     'quiz_id' => 1,
-        //     'question_id' => 1,
-        // ]);
-        //  DB::table('question_quiz')->insert([
-        //     'quiz_id' => 1,
-        //     'question_id' => 2,
-        // ]);
-        //  DB::table('question_quiz')->insert([
-        //     'quiz_id' => 1,
-        //     'question_id' => 3,
-        // ]);
+         DB::table('question_quiz')->insert([
+            'quiz_id' => 5,
+            'question_id' => 15,
+        ]);
+         DB::table('question_quiz')->insert([
+            'quiz_id' => 5,
+            'question_id' => 25,
+        ]);
+         DB::table('question_quiz')->insert([
+            'quiz_id' => 5,
+            'question_id' => 35,
+        ]);
 
-        //  DB::table('question_quiz')->insert([
-        //     'quiz_id' => 2,
-        //     'question_id' => 4,
-        // ]);
-        //  DB::table('question_quiz')->insert([
-        //     'quiz_id' => 2,
-        //     'question_id' => 5,
-        // ]);
-        //  DB::table('question_quiz')->insert([
-        //     'quiz_id' => 2,
-        //     'question_id' => 6,
-        // ]);
+         DB::table('question_quiz')->insert([
+            'quiz_id' => 15,
+            'question_id' => 45,
+        ]);
+         DB::table('question_quiz')->insert([
+            'quiz_id' => 15,
+            'question_id' => 55,
+        ]);
+         DB::table('question_quiz')->insert([
+            'quiz_id' => 15,
+            'question_id' => 65,
+        ]);
 
         
 
