4f15badf6024d504573a8eea9f9a1da686418e2a
Paulius Jarutis <76951786+PJarrr@users.noreply.github.com>
2021-10-31 13:46:09 +0200

    question, quiz seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,15 +16,15 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
          DB::table('users')->insert([
-            'name' => 'admin',
-            'email' => 'admin@admin.lt',
+            'name' => 'user1',
+            'email' => 'user1@user.lt',
              'password' => Hash::make('123'),
             
         ]);
 
         DB::table('users')->insert([
-            'name' => 'neAdmin',
-            'email' => 'neadmin@admin.lt',
+            'name' => 'user2',
+            'email' => 'user2@user.lt',
              'password' => Hash::make('123'),
            
         ]);
@@ -32,78 +32,78 @@ class DatabaseSeeder extends Seeder
          DB::table('questions')->insert([
             
             'user_id' => 5,
-            'question_text'=>'Kas?', 
-            'correct_answer'=>'correct1',
-            'incorrect_answer1'=>'2',
-            'incorrect_answer2'=>'3',
-            'incorrect_answer3'=>'4',
+            'question_text'=>'Capital of USA', 
+            'correct_answer'=>'Washington',
+            'incorrect_answer1'=>'New York',
+            'incorrect_answer2'=>'Los Angeles',
+            'incorrect_answer3'=>'Denver',
             
         ]);
          DB::table('questions')->insert([
             
             'user_id' => 5,
-            'question_text'=>'Kodel?', 
-            'correct_answer'=>'correct2',
-            'incorrect_answer1'=>'2',
-            'incorrect_answer2'=>'3',
-            'incorrect_answer3'=>'4',
+            'question_text'=>'Capital of Poland?', 
+            'correct_answer'=>'Washawa',
+            'incorrect_answer1'=>'Krakow',
+            'incorrect_answer2'=>'Gdansk',
+            'incorrect_answer3'=>'Vilnius',
             
         ]);
          DB::table('questions')->insert([
             
             'user_id' => 5,
-            'question_text'=>'Kaip?', 
-            'correct_answer'=>'correct3',
-            'incorrect_answer1'=>'2',
-            'incorrect_answer2'=>'3',
-            'incorrect_answer3'=>'4',
+            'question_text'=>'Capital of Germany?', 
+            'correct_answer'=>'Berlin',
+            'incorrect_answer1'=>'Munich',
+            'incorrect_answer2'=>'Frankfurt',
+            'incorrect_answer3'=>'Hamburg',
             
         ]);
 
          DB::table('questions')->insert([
             
             'user_id' => 15,
-            'question_text'=>'Kas?', 
-            'correct_answer'=>'correct1',
-            'incorrect_answer1'=>'2',
-            'incorrect_answer2'=>'3',
-            'incorrect_answer3'=>'4',
+            'question_text'=>'Who is current president of the USA?', 
+            'correct_answer'=>'Joe Biden',
+            'incorrect_answer1'=>'Barack Obama',
+            'incorrect_answer2'=>'Kamala Harris',
+            'incorrect_answer3'=>'Donald Trump',
             
         ]);
          DB::table('questions')->insert([
             
             'user_id' => 15,
-            'question_text'=>'Kodel?', 
-            'correct_answer'=>'correct2',
-            'incorrect_answer1'=>'2',
-            'incorrect_answer2'=>'3',
-            'incorrect_answer3'=>'4',
+            'question_text'=>'Who is Jim Carray?', 
+            'correct_answer'=>'Actor',
+            'incorrect_answer1'=>'Rock star',
+            'incorrect_answer2'=>'Politic',
+            'incorrect_answer3'=>'Pet detective',
             
         ]);
          DB::table('questions')->insert([
             
             'user_id' => 15,
-            'question_text'=>'Kaip?', 
-            'correct_answer'=>'correct3',
-            'incorrect_answer1'=>'2',
-            'incorrect_answer2'=>'3',
-            'incorrect_answer3'=>'4',
+            'question_text'=>'Who is the founder of Laravel?', 
+            'correct_answer'=>'Taylor Otwel',
+            'incorrect_answer1'=>'Alan Turing',
+            'incorrect_answer2'=>'Ada Lovelace',
+            'incorrect_answer3'=>'Larry Page',
             
         ]);
 
          DB::table('quizzes')->insert([
             'user_id' => 5,
-            'title' => 'quiz1',
+            'title' => 'Capitals',
             'password'=>'123',
-            'description' =>'test quiz',
-            'time'=>1,
+            'description' =>'test quiz about coutry capital cities',
+            'time'=>2,
         ]);
          DB::table('quizzes')->insert([
             'user_id' => 15,
-            'title' => 'quiz2',
+            'title' => 'People',
             'password'=>'123',
-            'description' =>'test quiz',
-            'time'=>1,
+            'description' =>'Test quiz about famous people',
+            'time'=>2,
         ]);
 
          DB::table('question_quiz')->insert([
